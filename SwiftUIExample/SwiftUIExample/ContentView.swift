b982d47d6282ea5dcd9b337b8f48816a65f83367
gaussbeam <koji.hanyu@studist.jp>
2023-08-15 17:26:19 +0900

    Add sample of CustomDialogView

--- SwiftUIExample/SwiftUIExample/ContentView.swift
+++ SwiftUIExample/SwiftUIExample/ContentView.swift
@@ -8,19 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var viewModel = ContentViewModel()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Image(systemName: "globe")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                Button("Show custom dialog") {
+                    viewModel.setNeedsToShowDialog()
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            if viewModel.isCustomDialogShowing {
+                customDialog
+            }
         }
-        .padding()
+    }
+    
+    var customDialog: some View {
+        CustomDialogView(delegate: viewModel) {
+            VStack(spacing: 12.0) {
+                Text("hello")
+                Button("close") {
+                    viewModel.setNeedsToHideDialog()
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var isCustomDialogShowing: Bool = false
+    
+    func setNeedsToShowDialog() {
+        isCustomDialogShowing = true
+    }
+    
+    func setNeedsToHideDialog() {
+        isCustomDialogShowing = false
+    }
+}
+
+extension ContentViewModel: CustomDialogViewDelegate {
+    func backgroundViewDidTap() {
+        setNeedsToHideDialog()
     }
 }
