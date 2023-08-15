584eb92ac07db02551400c54ce2f5726402a67ab
gaussbeam <koji.hanyu@studist.jp>
2023-08-15 17:19:19 +0900

    Add SwiftUI Example

---
+++ SwiftUIExample/SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by HANYU, Koji on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
